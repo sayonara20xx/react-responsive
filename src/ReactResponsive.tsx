5da06c5eef59cf2132575dc92390e861d650534c
sayonara20xx <danilka.andreev.123@gmail.com>
2022-08-07 18:36:43 +0300

    Убрал возможность ничего не передавать хуку, по другому обработал неиспользуемый для построения mediaQuery children, вместо длинной строчки с перечислением опциональных ключей для RequireAtLeastOne задал выражение, которое просто возьмет те же ключи через исключение children

--- src/ReactResponsive.tsx
+++ src/ReactResponsive.tsx
@@ -1,15 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
 interface UseMediaQueryProps {
-  query: string | null;
+  query: string;
 }
 
 function useMediaQuery(queryObject: UseMediaQueryProps) {
   const [isQueryMatching, setIsQueryMatching] = useState(() => {
-    if (queryObject.query !== null) {
-      return window.matchMedia(queryObject.query).matches;
-    }
-    return false;
+    return window.matchMedia(queryObject.query).matches;
   });
 
   useEffect(() => {
@@ -54,7 +51,7 @@ type RequireAtLeastOne<T, Keys extends keyof T = keyof T> =
   [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>
 }[Keys];
 
-type MediaQueryProps = RequireAtLeastOne<MediaQueryPropsTemplate, 'orientation' | 'minResolution' | 'maxResolution' | 'minWidth' | 'maxWidth' | 'minHeight' | 'maxHeight'>;
+type MediaQueryProps = RequireAtLeastOne<MediaQueryPropsTemplate, Exclude<keyof MediaQueryPropsTemplate, 'children'>>;
 
 const MediaQuery: (props: MediaQueryProps) => JSX.Element | null = (props) => {
   const [isQueryMatching, setIsQueryMatching] = useState(false);
@@ -69,18 +66,16 @@ const MediaQuery: (props: MediaQueryProps) => JSX.Element | null = (props) => {
     maxHeight: ['max-height', 'px'],
   };
 
-  const getStringQuery = () : string | null=> {
-    let strings = Object.entries(props).map((property)=>{
-      if (property[0] !== 'children') {
-        const nameAndUnit: string[] = queriesPropNameAndUnit[property[0]];
-        return `(${nameAndUnit[0]}: ${property[1]}${nameAndUnit[1]})`;
-      }
-      return null;
+  const getStringQuery = () : string => {
+    let strings = Object.entries(props)
+      .filter((elem) => {return elem[0] !== 'children';})
+      .map((property) => {
+      const nameAndUnit: string[] = queriesPropNameAndUnit[property[0]];
+      return `(${nameAndUnit[0]}: ${property[1]}${nameAndUnit[1]})`;
     });
 
-    strings = strings.filter(elem => { return elem !== null; });
     return strings.reduce((a, b) => {
-      return a + ' and ' + b;
+      return [a, b].join(' ');
     });
   };
 
@@ -97,7 +92,7 @@ const MediaQuery: (props: MediaQueryProps) => JSX.Element | null = (props) => {
     if (typeof(props.children) === 'function') return props.children(isQueryMatching);
     if (props.children && isQueryMatching) return props.children;
 
-    return null
+    return null;
   };
 
   return <>{renderChildren()}</>;
