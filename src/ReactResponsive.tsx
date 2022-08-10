f17116f7184424abf1c0493ea994c65bc17c57a8
sayonara20xx <danilka.andreev.123@gmail.com>
2022-08-10 19:38:20 +0300

    Нормально организовал работу с children, убрал тот страшный способ с доп массивом для преобразования имени свойства в имя для mediaQuery, поправил типизацию и мелкие косяки

--- src/ReactResponsive.tsx
+++ src/ReactResponsive.tsx
@@ -22,7 +22,7 @@ function useMediaQuery(queryObject: UseMediaQueryProps) {
 }
 
 type RenderProps = (matches: boolean) => JSX.Element;
-type ChildrenType = JSX.Element | JSX.Element[] | string | RenderProps | null;
+type ChildrenType = React.ReactNode | RenderProps;
 
 interface MediaQueryPropsTemplate {
   orientation?: 'portrait' | 'landscape';
@@ -35,16 +35,6 @@ interface MediaQueryPropsTemplate {
   children: ChildrenType;
 }
 
-interface QueryNames {
-  [orientation: string]: string[];
-  minResolution: string[];
-  maxResolution: string[];
-  minWidth: string[];
-  maxWidth: string[];
-  minHeight: string[];
-  maxHeight: string[];
-}
-
 type RequireAtLeastOne<T, Keys extends keyof T = keyof T> =
   Pick<T, Exclude<keyof T, Keys>>
   & {
@@ -53,30 +43,22 @@ type RequireAtLeastOne<T, Keys extends keyof T = keyof T> =
 
 type MediaQueryProps = RequireAtLeastOne<MediaQueryPropsTemplate, Exclude<keyof MediaQueryPropsTemplate, 'children'>>;
 
-const MediaQuery: (props: MediaQueryProps) => JSX.Element | null = (props) => {
+const MediaQuery: (props: MediaQueryProps) => JSX.Element | null = ({ children, ...props }) => {
   const [isQueryMatching, setIsQueryMatching] = useState(false);
 
-  const queriesPropNameAndUnit : QueryNames = {
-    orientation: ['orientation', ''],
-    minResolution: ['min-resolution', 'dppx'],
-    maxResolution: ['max-resolution', 'dppx'],
-    minWidth: ['min-width', 'px'],
-    maxWidth: ['max-width', 'px'],
-    minHeight: ['min-height', 'px'],
-    maxHeight: ['max-height', 'px'],
-  };
-
-  const getStringQuery = () : string => {
+  const getStringQuery = () => {
     let strings = Object.entries(props)
-      .filter((elem) => {return elem[0] !== 'children';})
-      .map((property) => {
-      const nameAndUnit: string[] = queriesPropNameAndUnit[property[0]];
-      return `(${nameAndUnit[0]}: ${property[1]}${nameAndUnit[1]})`;
-    });
+      .map(([key, value]) => {
+        const name = key.replace(/[A-Z]/, (match) => {return '-' + match.toLowerCase(); });
+        let unit = '';
 
-    return strings.reduce((a, b) => {
-      return [a, b].join(' ');
-    });
+        if (name.includes('resolution')) unit = 'dppx';
+        if (name.includes('width') || name.includes('height')) unit = 'px';
+
+        return `(${name}: ${value}${unit})`;
+      });
+
+    return strings.join(' and ');
   };
 
   const queryString = getStringQuery();
@@ -89,8 +71,8 @@ const MediaQuery: (props: MediaQueryProps) => JSX.Element | null = (props) => {
   }, [isMatched, props]);
 
   const renderChildren = (): ChildrenType => {
-    if (typeof(props.children) === 'function') return props.children(isQueryMatching);
-    if (props.children && isQueryMatching) return props.children;
+    if (typeof(children) === 'function') return children(isQueryMatching);
+    if (children && isQueryMatching) return children;
 
     return null;
   };
